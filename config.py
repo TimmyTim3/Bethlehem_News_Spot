89f0ede6615c2e77bd529b2afd6b52fdb9216068
Tim <286092996+TimmyTim3@users.noreply.github.com>
2026-07-04 10:07:55 +0200

    Phase 2.2 Dashboard Foundation

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "change-this-later"
+
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(BASE_DIR, "database", "news.db")
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
