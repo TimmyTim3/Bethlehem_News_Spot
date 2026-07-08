679b815e78d30b0595898cd443bf2a74cefeca23
Tim <286092996+TimmyTim3@users.noreply.github.com>
2026-07-08 03:58:53 +0200

    Phase 4.1 Breaking News

--- routes/home.py
+++ routes/home.py
@@ -1,15 +1,22 @@
 from flask import Blueprint, render_template
 
-from services.article_service import get_all_articles
+from services.article_service import (
+    get_all_articles,
+    get_featured_articles,
+    get_breaking_articles,
+)
 
 home_bp = Blueprint("home", __name__)
-
 
 @home_bp.route("/")
 def index():
     articles = get_all_articles()
+    featured_articles = get_featured_articles()
+    breaking_articles = get_breaking_articles()
 
     return render_template(
-        "index.html",
-        articles=articles
-    )
+    "index.html",
+    articles=articles,
+    featured_articles=featured_articles,
+    breaking_articles=breaking_articles,
+)
